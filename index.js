a8b9e49e332f34396719d469e95e5dd14b680fde
saswatsam786 <saswat.sam786@gmail.com>
2022-10-23 23:08:45 +0530

    docker compose with 2 services

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World3');
+    res.send('<h2>Hello World</h2>');
 });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
-// 1:32:38
+// 1:53:43
